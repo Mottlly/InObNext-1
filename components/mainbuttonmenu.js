b403024f0c8d34aad078ff67452ebc7a03d49fd8
Mottlly <147268663+Mottlly@users.noreply.github.com>
2024-04-11 15:41:34 -0300

    refactor(in-ob):[Tommy]: remove useless file

--- components/mainbuttonmenu.js
+++ components/mainbuttonmenu.js
@@ -8,9 +8,9 @@ function MainMenuButtons() {
       <div id="mainbuttons" className="mainbuttons">
         <ImageButton
           imageUrl="/play.png"
-          href="nextjs-blog\pages\maingame.js"
+          href="nextjs-blog/pages/maingame.js"
         />
-        <ImageButton imageUrl="/burger.png" href="nextjs-blog\pages\index.js" />
+        <ImageButton imageUrl="/burger.png" href="nextjs-blog/pages/index.js" />
         <ImageButton imageUrl="/cog.png" href="nil" />
       </div>
     </>
