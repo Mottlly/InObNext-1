5e228149c6bd83148e4beb567d9baea1de91a91f
Mottlly <147268663+Mottlly@users.noreply.github.com>
2024-06-24 20:20:25 -0300

    ph3 be start

--- nextjs-blog/components/decisioncard.js
+++ nextjs-blog/components/decisioncard.js
@@ -4,15 +4,18 @@ import Textbox from "../components/textbox";
 import styles from "../styles/decisioncard.module.scss";
 import CardImage from "../components/cardimage";
 import TypingTextBox from "../components/typingtextbox";
+import dbMocks from "../__mocks__/dbMocks.json";
 
 export default function DecisionCard() {
+  console.log(dbMocks);
+  const text = dbMocks[0].text;
   return (
     <>
       <Head>
         <title>Decision Card Container</title>
       </Head>
       <div id="decisioncardcontainer" className={styles.CardAlpha}>
-        <TypingTextBox text="I Am Here..... And I can type myself!" />
+        <TypingTextBox text={text} />
         <CardImage imageUrl="/pixelholder.jpg" />
       </div>
     </>
