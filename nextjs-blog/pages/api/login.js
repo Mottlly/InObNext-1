3ef47c1618a1266af0e82f42972fa672d1f1ea8a
Mottlly <147268663+Mottlly@users.noreply.github.com>
2024-07-15 19:20:29 -0300

    chore(in-ob):[ph4]:Clean Up Notes

--- nextjs-blog/pages/api/login.js
+++ nextjs-blog/pages/api/login.js
@@ -4,16 +4,13 @@
 
 // pages/api/login.js
 
-// Example endpoint to handle login
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method Not Allowed" });
   }
 
-  // Perform authentication logic here (e.g., check credentials, generate tokens, etc.)
   const { email, password } = req.body;
 
-  // Example: Check if email and password match (replace with your actual authentication logic)
   if (email === "user@example.com" && password === "password") {
     // Mock user data
     const user = {
@@ -22,7 +19,6 @@ export default async function handler(req, res) {
       email: "user@example.com",
     };
 
-    // Ideally, generate a JWT token here and send it back in response
     return res.status(200).json({ user });
   } else {
     return res.status(401).json({ message: "Unauthorized" });
