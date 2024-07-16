7a46cb4eba8f5c80ec60207c9383918dc110dc30
Mottlly <147268663+Mottlly@users.noreply.github.com>
2024-07-16 16:42:32 -0300

    WAAAAAGH

--- nextjs-blog/pages/api/login.js
+++ nextjs-blog/pages/api/login.js
@@ -4,23 +4,42 @@
 
 // pages/api/login.js
 
+//current mock login: 1example@example.com - steven
+import { MongoClient } from "mongodb";
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method Not Allowed" });
   }
 
   const { email, password } = req.body;
+  console.log(process.env.MONGODB_URI);
+  const client = new MongoClient(process.env.MONGODB_URI);
 
-  if (email === "user@example.com" && password === "password") {
-    // Mock user data
-    const user = {
-      id: 1,
-      username: "user",
-      email: "user@example.com",
-    };
+  try {
+    await client.connect();
+    // Choose a name for your database
+    const database = client.db("InObscurum");
 
-    return res.status(200).json({ user });
-  } else {
-    return res.status(401).json({ message: "Unauthorized" });
+    // Choose a name for your collection
+    const collection = database.collection("Users");
+
+    // Example: Find a user by email and password
+    console.log("email:", email);
+    console.log("password:", password);
+    const user = await collection.findOne({
+      email: email,
+      password: password,
+    });
+    if (user) {
+      res.status(200).json({ message: "Authorized" });
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+    res.status(500).json({ message: "Something went wrong!" });
+  } finally {
+    await client.close();
   }
 }
