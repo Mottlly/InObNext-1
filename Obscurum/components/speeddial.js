d6170d568a07a1078e806279665f397cf2917401
Mottlly <147268663+Mottlly@users.noreply.github.com>
2024-08-17 12:41:04 -0300

    chore(in-ob):[ph4][IPC]:rename files camel

--- Obscurum/components/speeddial.js
+++ Obscurum/components/speeddial.js
@@ -1,16 +1,16 @@
 import * as React from "react";
 import { useState } from "react";
 import Box from "@mui/material/Box";
-import FullscreenOverlay from "../components/transoverlay";
+import FullscreenOverlay from "./transOverlay";
 import { SpeedDial, SpeedDialIcon, SpeedDialAction } from "@mui/material";
 import PlayArrow from "@mui/icons-material/PlayArrow";
 import Copyright from "@mui/icons-material/Copyright";
 import LocalMovies from "@mui/icons-material/LocalMovies";
 
 const actions = [
-  { icon: <PlayArrow />, name: "MainGame", href: "/maingame" },
-  { icon: <LocalMovies />, name: "Movie", href: "/menu" },
-  { icon: <Copyright />, name: "Credits", href: "/credits" },
+  { icon: <PlayArrow />, name: "MainGame", href: "/mainGame" },
+  { icon: <LocalMovies />, name: "Movie", href: "/Menu" },
+  { icon: <Copyright />, name: "Credits", href: "/Credits" },
   {
     icon: <FullscreenOverlay />,
     name: "Settings",
--- Obscurum/pages/menu.js
+++ Obscurum/pages/menu.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import OpeningCrawl from "./opener";
+import OpeningCrawl from "./Opener";
 
 export default function Home() {
   return (
